efc3e2ed06d2f474726196315fcc55925fbfa53f
babakoto <e.tokiniaina@gmail.com>
2022-07-30 18:49:51 +0200

    first commit

---
+++ lib/flutter_boot.dart
@@ -0,0 +1,13 @@
+library flutter_boot;
+
+export "./flutter_boot.dart";
+export 'package:flutter_bloc/flutter_bloc.dart';
+export 'package:easy_localization/easy_localization.dart';
+export 'package:connectivity_plus/connectivity_plus.dart';
+export 'package:internet_connection_checker/internet_connection_checker.dart';
+export 'package:intl/intl.dart';
+export 'package:isar/isar.dart';
+export 'package:flutter_form_builder/flutter_form_builder.dart';
+export 'package:equatable/equatable.dart';
+export 'package:get_it/get_it.dart';
+export 'package:multiple_result/multiple_result.dart';
---
+++ test/flutter_boot_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:flutter_boot/flutter_boot.dart';
+
+void main() {
+  test('adds one to input values', () {});
+}
